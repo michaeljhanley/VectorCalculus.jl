d155eeb01331bb16695906da25aa2a73bf88f1ef
Michael Hanley <152537546+michaeljhanley@users.noreply.github.com>
2026-07-09 17:02:55 -0700

    First pass at coordinate_systems.jl

---
+++ src/coordinate_systems.jl
@@ -0,0 +1,38 @@
+abstract type CoordSystem end
+
+struct CurvilinearCoords{H1Function, H2Function, H3Function} <: CoordSystem
+    h1_function::H1Function
+    h2_function::H2Function
+    h3_function::H3Function
+end
+
+function scale_factors(coordinate_system, point)
+    h1 = coordinate_system.h1_function(point)
+    h2 = coordinate_system.h2_function(point)
+    h3 = coordinate_system.h3_function(point)
+    (h1, h2, h3)
+end
+
+function cartesian()
+    CurvilinearCoords(
+        point -> 1,
+        point -> 1,
+        point -> 1,
+    )
+end
+
+function cylindrical()
+    CurvilinearCoords(
+        point -> 1,
+        point -> point[1],
+        point -> 1
+    )
+end
+
+function spherical()
+    CurvilinearCoords(
+        point -> 1,
+        point -> point[1],
+        point -> point[1] * sin(point[2])
+    )
+end
